517a3bb097318fe12bdab11ca0dd3bb2907e7b89
wiladhianty <wiladhianty@gmail.com>
2021-07-06 12:31:27 +0700

    tambah tugas

---
+++ tugas1.php
@@ -0,0 +1,13 @@
+<?php
+    $beli = 8000;
+    $jual = 7500;
+    $untung = 300;
+    $kambing = null;
+    echo "
+        Harga Beli = Rp $beli <br/>
+        Harga Jual = Rp $jual <br/>
+        Keuntungan = Rp $untung <br/>
+        Berapa Jumlah Kambing yang dibeli? <br/>
+        Jawaban : $kambing ekor
+    ";
+?>
---
+++ tugas2.php
@@ -0,0 +1,11 @@
+<?php
+    $saluran = 523;
+    $air = 8891;
+    $rata = $air/$saluran;
+    echo "
+        Jumlah Saluran = $saluran <br/>
+        Jumlah terpakai = $air <br/>
+        Rata-rata pemakaian = ? liter/keluarga <br/>
+        Jawaban : $rata liter/keluarga
+    ";
+?>
